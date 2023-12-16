8307e92d91d9d155ed481ac1f4432f0131c49840
Mahendra Varma <mahendrabuddharaju3886@gmail.com>
2023-12-16 12:25:36 +0530

    Developed sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedSidebarComponent } from './shared-sidebar/shared-sidebar.component';
+import { CreateGroupComponent } from './create-group/create-group.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SharedSidebarComponent,
+    CreateGroupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from "@angular/router";
+import { CreateGroupComponent } from "./create-group/create-group.component";
+
+export const appRoutes:Routes = [
+    {path:'',component:CreateGroupComponent}
+]
